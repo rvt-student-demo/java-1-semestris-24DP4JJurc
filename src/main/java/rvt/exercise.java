c897e01896227ef6843856e64d766d53efef7eb4
24DP4JJurc <A240102JJ@rvt.lv>
2025-10-17 12:43:11 +0300

    update exercise

--- src/main/java/rvt/exercise.java
+++ src/main/java/rvt/exercise.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class exercise {
     public static void main(String[] args) {
-        ex1();
+        // You can switch between ex1(), ex2(), ex3()
+        ex3();
     }
 
     public static void ex1() {
@@ -15,11 +16,54 @@ public class exercise {
         System.out.println("Enter End:");
         int max = scanner.nextInt();
 
-        for ( int i = min; i <= max; i++) {
+        for (int i = min; i <= max; i++) {
             System.out.println(i);
         }
 
         scanner.close();
     }
-}
 
+    public static void ex2() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter N:");
+        int n = scanner.nextInt();
+
+        int loopSum = 0;
+        for (int i = 1; i <= n; i++) {
+            loopSum += i;
+        }
+
+        int formulaSum = (n * (n + 1)) / 2;
+
+        System.out.println("Loop Sum = " + loopSum);
+        System.out.println("Formula Sum = " + formulaSum);
+
+        scanner.close();
+    }
+
+    public static void ex3() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter N:");
+        int n = scanner.nextInt();
+
+        
+        int loopSumSquares = 0;
+        for (int i = 1; i <= n; i++) {
+            loopSumSquares += i * i;
+        }
+
+       
+        int formulaSumSquares = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        
+        int formulaSumCubes = (int) Math.pow((n * (n + 1)) / 2, 2);
+
+        System.out.println("Loop Sum of Squares = " + loopSumSquares);
+        System.out.println("Formula Sum of Squares = " + formulaSumSquares);
+        System.out.println("Formula Sum of Cubes = " + formulaSumCubes);
+
+        scanner.close();
+    }
+}
